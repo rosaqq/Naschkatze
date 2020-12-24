ac00cb093b25633e8a3a5c846f3f4cf4fb0a6d4c
Tiago Rosa <tcrosa27@gmail.com>
2020-12-24 15:55:46 +0000

    fix indent to 4 spaces

--- src/main/java/net/secknv/nkmod/Naschkatze.java
+++ src/main/java/net/secknv/nkmod/Naschkatze.java
@@ -15,34 +15,34 @@ public class Naschkatze {
 
 
     public static final String MODID = "nkmod";
-	private static final Logger LOGGER = LogManager.getLogger();
+    private static final Logger LOGGER = LogManager.getLogger();
 
-	public static final ItemGroup TAB = new ItemGroup("tabNk") {
-		@Override
-		public ItemStack createIcon() {
-			return new ItemStack(RegistryHandler.URANIUM_INGOT.get());
-		}
-	};
+    public static final ItemGroup TAB = new ItemGroup("tabNk") {
+        @Override
+        public ItemStack createIcon() {
+            return new ItemStack(RegistryHandler.URANIUM_INGOT.get());
+        }
+    };
 
-	public Naschkatze() {
-		// Register the setup method for modloading
-		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
-		// Register the doClientStuff method for modloading
-		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+    public Naschkatze() {
+        // Register the setup method for modloading
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        // Register the doClientStuff method for modloading
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
 
-		// Register ourselves for server and other game events we are interested in
-		MinecraftForge.EVENT_BUS.register(this);
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
 
-		RegistryHandler.init();
-	}
+        RegistryHandler.init();
+    }
 
-	private void setup(final FMLCommonSetupEvent event) {
-		// pre init code
-		LOGGER.info("nkmod pre init");
-	}
+    private void setup(final FMLCommonSetupEvent event) {
+        // pre init code
+        LOGGER.info("nkmod pre init");
+    }
 
-	private void doClientStuff(final FMLClientSetupEvent event) {
-		// do something that can only be done on the client
-		LOGGER.info("Got game settings {}", event.getMinecraftSupplier().get().gameSettings);
-	}
+    private void doClientStuff(final FMLClientSetupEvent event) {
+        // do something that can only be done on the client
+        LOGGER.info("Got game settings {}", event.getMinecraftSupplier().get().gameSettings);
+    }
 }
