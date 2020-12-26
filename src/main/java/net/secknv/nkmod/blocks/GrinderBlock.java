6353822c3840bea1c52a093b3bbbc6b124c97e51
Tiago Rosa <tcrosa27@gmail.com>
2020-12-26 01:29:01 +0000

    added grinder rotation on placement

--- src/main/java/net/secknv/nkmod/blocks/GrinderBlock.java
+++ src/main/java/net/secknv/nkmod/blocks/GrinderBlock.java
@@ -1,10 +1,30 @@
 package net.secknv.nkmod.blocks;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.HorizontalBlock;
 import net.minecraft.block.material.Material;
+import net.minecraft.item.BlockItemUseContext;
+import net.minecraft.state.DirectionProperty;
+import net.minecraft.state.StateContainer;
+import net.minecraft.util.Direction;
+import net.minecraft.util.Mirror;
+import net.minecraft.util.Rotation;
 
 public class GrinderBlock extends Block {
+
+    public static final DirectionProperty FACING = HorizontalBlock.HORIZONTAL_FACING;
+
     public GrinderBlock() {
         super(Block.Properties.create(Material.IRON));
+        this.setDefaultState(this.stateContainer.getBaseState().with(FACING, Direction.NORTH));
+    }
+
+    public BlockState getStateForPlacement(BlockItemUseContext context) {
+        return this.getDefaultState().with(FACING, context.getPlacementHorizontalFacing().getOpposite());
+    }
+
+    protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
+        builder.add(FACING);
     }
 }
