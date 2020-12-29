afd94acb72a140c98ed41732d21f51f9866ea878
Tiago Rosa <tcrosa27@gmail.com>
2020-12-29 16:53:01 +0000

    Added grinder container

---
+++ src/main/java/net/secknv/nkmod/blocks/GrinderBlockContainer.java
@@ -0,0 +1,76 @@
+package net.secknv.nkmod.blocks;
+
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.inventory.container.Container;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.IWorldPosCallable;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraftforge.items.CapabilityItemHandler;
+import net.minecraftforge.items.IItemHandler;
+import net.minecraftforge.items.SlotItemHandler;
+import net.minecraftforge.items.wrapper.InvWrapper;
+
+import static net.secknv.nkmod.RegistryHandler.GRINDER;
+import static net.secknv.nkmod.RegistryHandler.GRINDER_CONTAINER;
+
+public class GrinderBlockContainer extends Container {
+
+    private TileEntity tileEntity;
+    private PlayerEntity playerEntity;
+    private IItemHandler playerInventory;
+
+    public GrinderBlockContainer(int windowId, World world, BlockPos pos, PlayerInventory playerInventory, PlayerEntity player) {
+        super(GRINDER_CONTAINER.get(), windowId);
+        tileEntity = world.getTileEntity(pos);
+        this.playerEntity = player;
+        this.playerInventory = new InvWrapper(playerInventory);
+
+        if (tileEntity != null) {
+            tileEntity.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY).ifPresent(h -> {
+                // Furnace input slot
+                addSlot(new SlotItemHandler(h, 0, 56, 17));
+                // Furnace fuel slot
+                // todo: item handler for fuel
+                //addSlot(new SlotItemHandler(h, 1, 56, 53));
+                // Furnace result slot
+                // todo: item handler / code for output
+                //addSlot(new SlotItemHandler(h, 2, 116, 35));
+
+            });
+        }
+
+        layoutPlayerInventorySlots(8, 84);
+    }
+
+    @Override
+    public boolean canInteractWith(PlayerEntity playerIn) {
+        return isWithinUsableDistance(IWorldPosCallable.of(tileEntity.getWorld(), tileEntity.getPos()), playerEntity, GRINDER.get());
+    }
+
+    private int addSlotRange(IItemHandler handler, int index, int x, int y, int amount, int dx) {
+        for (int i = 0; i < amount; i++) {
+            addSlot(new SlotItemHandler(handler, index, x, y));
+            x+=dx;
+            index++;
+        }
+        return index;
+    }
+
+    private int addSlotBox(IItemHandler handler, int index, int x, int y, int horAmount, int dx, int verAmount, int dy) {
+        for (int j = 0; j < verAmount; j++) {
+            index = addSlotRange(handler, index, x, y, horAmount, dx);
+            y+=dy;
+        }
+        return index;
+    }
+
+    private void layoutPlayerInventorySlots(int leftCol, int topRow) {
+        // Player inventory
+        addSlotBox(playerInventory, 9, leftCol, topRow, 9, 18, 3, 18);
+        // Hotbar
+        topRow += 58;
+        addSlotRange(playerInventory, 0, leftCol, topRow, 9, 18);
+    }
+}
