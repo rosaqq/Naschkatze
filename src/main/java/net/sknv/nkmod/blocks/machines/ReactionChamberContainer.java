8b94b9efc878657455798ef5a189f39046a481c1
Tiago Rosa <tcrosa27@gmail.com>
2021-01-19 00:55:10 +0000

    adding TExp sulfur to ReactionChamber ItemHandler

--- src/main/java/net/sknv/nkmod/blocks/machines/ReactionChamberContainer.java
+++ src/main/java/net/sknv/nkmod/blocks/machines/ReactionChamberContainer.java
@@ -9,6 +9,7 @@ import net.minecraft.world.World;
 import net.minecraftforge.common.util.NonNullConsumer;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.items.IItemHandler;
+import net.minecraftforge.items.SlotItemHandler;
 import net.sknv.nkmod.blocks.machines.base.AbstractMachineContainer;
 
 import javax.annotation.Nullable;
@@ -23,6 +24,16 @@ public class ReactionChamberContainer extends AbstractMachineContainer {
 
     @Override
     public NonNullConsumer<IItemHandler> itemHandlerConsumerProvider() {
-        return null;
+        return h -> {
+            // Furnace input slot
+            addSlot(new SlotItemHandler(h, 0, 56, 17));
+            // Furnace fuel slot
+            // todo: item handler for fuel
+            //addSlot(new SlotItemHandler(h, 1, 56, 53));
+            // Furnace result slot
+            // todo: item handler / code for output
+            //addSlot(new SlotItemHandler(h, 2, 116, 35));
+
+        };
     }
 }
