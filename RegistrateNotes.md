1df6d21a05914bc33133bb4163ca14e297145fa8
Tiago Rosa <tcrosa27@gmail.com>
2021-01-23 04:13:52 +0000

    cleaned up registrate implementation

--- RegistrateNotes.md
+++ RegistrateNotes.md
@@ -1,23 +1,63 @@
 # How to migrate from Forge registry to using Registrate
 
-## Init Registrate
-```java
-REGISTRATE = Registrate.create(MODID).itemGroup(()-> new ItemGroup("Tab Label") {
-    @Nonnull
-    @Override
-    public ItemStack createIcon() {
-        return new ItemStack(MY_ITEM.get());
-    }
-});
-```
-Notes:
-- `Registrate.create()` must be called at main mod class constructor or later. Static init is too early.  
-   Good example TBD
+## Stuff Registrate does for you
+- Linking Screens to Containers   
+  `REGISTRATE.container(MyMachineContainer::new, MyMachineScreen::new)`
+  instead of having something like:
   ```java
-   Good example placeholder
+  @Mod.EventBusSubscriber(modid = MODID, value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.MOD)
+  public class ClientSetup {
+  
+    public static void init(final FMLClientSetupEvent event) {
+        ScreenManager.registerFactory(RegistryHandler.MY_MACHINE_CONTAINER.get(), MyMachineScreen::new);
+        ScreenManager.registerFactory(RegistryHandler.MY_OTHER_MACHINE_CONTAINER.get(), MyOtherMachineScreen::new);
+    }
+  }
   ```
-- By calling .itemGroup() like this, every Mod Item gets automatically added to the creative tab.
-  You could also pass a pre-made itemGroup subclass (and include a localized name): `.itemGroup(MyModItemGroup::new, "Localized Name")`
+- Automatically puts all your stuff into the creative tab you specify during `Registrate.create()`
+- Simple BlockItem generation with `.block().simpleItem()`
+- Simple TE registration with `.block().simpleTileEntity()`
+- Datagen (TBD)
+- Lang (TBD)
+
+## Init Registrate
+- `Registrate.create()` must be called at main mod class constructor or later. Static init is too early.  
+   Here is an example where we create a helper class, declare static fields, and fill them up later (`RegistrateHandler.init()` gets called on our @Mod class' constructor)  
+  ```java
+  public class RegistrateHandler {
+  
+    public static Registrate REGISTRATE;
+  
+    // blocks
+    public static RegistryEntry<MyBlock> MY_BLOCK;
+    public static RegistryEntry<MyMachineBlock> MY_MACHINE_BLOCK;
+    public static RegistryEntry<ContainerType<MyMachineContainer>> MY_MACHINE_CONTAINER;
+  
+    // items
+    public static RegistryEntry<Item> A_ITEM;
+    public static RegistryEntry<Item> B_ITEM;
+  
+    public static void init() {
+  
+      REGISTRATE = Registrate.create(Naschkatze.MODID).itemGroup(MyCreativeTab::new);
+  
+      // blocks
+      MY_BLOCK = REGISTRATE.object("my_block").block(MyBlock::new)
+              .properties(p -> p.harvestLevel(2))
+              .simpleItem().register();
+      MY_MACHINE_BLOCK = REGISTRATE.object("my_machine").block(Material.IRON, MyMachineBlock::new)
+              .simpleTileEntity(MyMachineTile::new)
+              .simpleItem().register();
+      MY_MACHINE_CONTAINER = REGISTRATE.container(MyMachineContainer::new, MyMachineScreen::new).register();
+  
+      // items
+      A_ITEM = REGISTRATE.object("a_item").item(Item::new).register();
+      B_ITEM = REGISTRATE.object("b_item").item(Item::new).register();
+    }
+  }
+  ```
+- By calling `.itemGroup()` like this, every Mod Item gets automatically added to the creative tab.
+  You could also pass a localized name: `.itemGroup(MyModItemGroup::new, "Localized Name")`
 
 ## Register a normal block
 ```java
@@ -27,7 +67,7 @@ becomes
 ```java
 RegistryEntry<MyBlock> MY_BLOCK = REGISTRATE.object("my_block").block(MyBlock::new).simpleItem().register();
 ```
-Note the .simpleItem() to add Block Items -> no need to have a separate `MY_BLOCK_ITEM`.  
+Note the `.simpleItem()` to add Block Items -> no need to have a separate `MY_BLOCK_ITEM`.  
 Get Block Items with `MY_BLOCK.get().asItem()`.  
 RegistryEntry is a more flexible IForgeRegistryEntry implementation by Registrate.  
 MyBlock must accept Properties in its constructor. If you want to add more hardcoded Properties, you can do it this way:
--- src/main/java/net/sknv/nkmod/RegistrateHandler.java
+++ src/main/java/net/sknv/nkmod/RegistrateHandler.java
@@ -9,7 +9,6 @@ import net.minecraft.inventory.container.ContainerType;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
-import net.minecraft.tileentity.TileEntityType;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.sknv.nkmod.blocks.NkOreBlock;
 import net.sknv.nkmod.blocks.machines.GrinderContainer;
@@ -23,12 +22,12 @@ public class RegistrateHandler {
 
     public static Registrate REGISTRATE;
 
-    // register blocks
+    // blocks
     public static RegistryEntry<NkOreBlock> URANINITE_ORE;
     public static RegistryEntry<MachineBlock> GRINDER;
-    public static RegistryEntry<TileEntityType<GrinderTile>> GRINDER_TILE;
     public static RegistryEntry<ContainerType<Container>> GRINDER_CONTAINER;
-    // register items
+
+    // items
     public static RegistryEntry<Item> CRUSHED_URANINITE;
     public static RegistryEntry<Item> YELLOWCAKE;
     public static RegistryEntry<Item> URANIUM_INGOT;
@@ -44,6 +43,8 @@ public class RegistrateHandler {
                 return new ItemStack(URANIUM_INGOT.get());
             }
         });
+
+        // blocks
         URANINITE_ORE = REGISTRATE.object("uraninite_ore").block(NkOreBlock::new).properties(p -> p.harvestLevel(2)).simpleItem().register();
         GRINDER = REGISTRATE.object("grinder").block(Material.IRON, p -> new MachineBlock(p, () -> new GrinderTile(GRINDER.getSibling(ForgeRegistries.TILE_ENTITIES).get())))
                 .simpleTileEntity(GrinderTile::new)
@@ -51,38 +52,12 @@ public class RegistrateHandler {
         // todo: GrinderContainer should not ignore type param
         // todo: data.readBlockPos() ok? NPE?
         GRINDER_CONTAINER = REGISTRATE.container((type, windowId, inv, data) -> new GrinderContainer(windowId, inv, data.readBlockPos()), () -> new MachineScreenFactory<>("textures/gui/grinder_gui.png")).register();
-        // register items
+
+        // items
         CRUSHED_URANINITE = REGISTRATE.object("crushed_uraninite").item(Item::new).register();
         YELLOWCAKE = REGISTRATE.object("yellowcake").item(Item::new).register();
         URANIUM_INGOT = REGISTRATE.object("uranium_ingot").item(Item::new).register();
         UO2_INGOT = REGISTRATE.object("uo2_ingot").item(Item::new).register();
         GRAPHITE_INGOT = REGISTRATE.object("graphite_ingot").item(Item::new).register();
     }
-
-
-
-    /*
-    // todo: document machine blocks abstract classes and interfaces
-    // todo: make wrapper method to add machines that adds Block, Tile, Container, BlockItem automatically.
-
-    public static final RegistryObject<Block> GRINDER = BLOCKS.register("grinder", () -> new MachineBlock(GrinderTile::new));
-    public static final RegistryObject<Block> REACTIONCHAMBER = BLOCKS.register("reaction_chamber", () -> new MachineBlock(ReactionChamberTile::new));
-
-
-    // register block items
-    public static final RegistryObject<Item> URANINITE_ORE_ITEM = ITEMS.register("uraninite_ore", () -> new NkBlockItem(URANINITE_ORE.get()));
-    public static final RegistryObject<Item> GRINDER_ITEM = ITEMS.register("grinder", () -> new NkBlockItem(GRINDER.get()));
-    public static final RegistryObject<Item> REACTIONCHAMBER_ITEM = ITEMS.register("reaction_chamber", () -> new NkBlockItem(REACTIONCHAMBER.get()));
-
-    // register block TEs
-    public static final RegistryObject<TileEntityType<GrinderTile>> GRINDER_TILE = TILES.register("grinder", () -> TileEntityType.Builder.create(GrinderTile::new, GRINDER.get()).build(null));
-    public static final RegistryObject<TileEntityType<ReactionChamberTile>> REACTIONCHAMBER_TILE = TILES.register("reaction_chamber", () -> TileEntityType.Builder.create(ReactionChamberTile::new, REACTIONCHAMBER.get()).build(null));
-
-    // register block containers
-    public static final RegistryObject<ContainerType<GrinderContainer>> GRINDER_CONTAINER = CONTAINERS.register("grinder", () -> IForgeContainerType.create((windowId, inv, data) -> new GrinderContainer(windowId, inv.player.getEntityWorld(), data.readBlockPos(), inv, inv.player)));
-    public static final RegistryObject<ContainerType<ReactionChamberContainer>> REACTIONCHAMBER_CONTAINER = CONTAINERS.register("reaction_chamber", () -> IForgeContainerType.create((windowId, inv, data) -> new ReactionChamberContainer(windowId, inv.player.getEntityWorld(), data.readBlockPos(), inv, inv.player)));
-*/
-
-
-
 }
